4ba12492962012f9a8e6e587ad1638356ea4d1ae
shforeal <rivena14798@gmail.com>
2023-04-07 11:23:48 +0300

    updaate: readmeconfig

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     # General Config
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret'
     FLASK_APP = os.environ.get('FLASK_APP') or 'app.py'
     FLASK_ENV = os.environ.get('FLASK_ENV') or 'development'
 
